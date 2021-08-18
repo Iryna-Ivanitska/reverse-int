3c144f512930de104d9ce54939c7dab4da5f188c
Ityna Ivanytska <ira.pechenitsya@gmal.com>
2021-08-18 09:20:34 +0300

    feat: add the function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+  let array = [],
+      sNum = n.toString();
+
+  for (let i = 0; i < sNum.length; i++) {
+      array.push(sNum.charAt(i));
+  } 
+
+  return parseInt(array.reverse().join(''));
 }
